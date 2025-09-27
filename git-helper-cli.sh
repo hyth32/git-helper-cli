4e21a29a34db0cbe4384ad62aa8636828776a0f5
Ivan Goryaev <5474n.sage@gmail.com>
2025-09-28 01:45:00 +0300

    вынес билд списка в метод

--- git-helper-cli.sh
+++ git-helper-cli.sh
@@ -5,29 +5,42 @@ if ! git rev-parse --is-inside-work-tree &>/dev/null; then
     exit 1
 fi
 
+build_fzf_menu() {
+    local header="$1"
+    local prompt="$2"
+    shift 2
+    printf "%s\n" "$@" | fzf \
+        --height 10 \
+        --border \
+        --reverse \
+        --header-first \
+        --header="$header" \
+        --prompt="$prompt"
+}
+
 current_branch=$(git symbolic-ref --short HEAD)
 
 show_main_menu() {
     actions=("Выбор ветки" "Последние коммиты" "Коммит" "Выход")
-    printf "%s\n" "${actions[@]}" | fzf --height 10 --border \
-        --reverse \
-        --header-first \
-        --header="Текущая ветка: $current_branch" \
-        --prompt="Выберите действие: "
+    build_fzf_menu \
+        "Текущая ветка: $current_branch" \
+        "Выберите действие: " \
+        "${actions[@]}"
 }
 
-choose_branch() {
+change_branch() {
     branches=$(git branch --all)
     branches="Назад"$'\n'"$branches"
-    
-    selected_branch=$(echo "$branches" | fzf --height 15 --border \
-        --reverse \
-        --header-first \
-        --header="Выбор ветки" \
-        --prompt="Выберите ветку: ")
+
+    selected_branch=$(
+        build_fzf_menu \
+        "Выбор ветки" \
+        "Выберите ветку: " \
+        "$branches"
+    )
     
     if [ -z "$selected_branch" ] || [ "$selected_branch" == "Назад" ]; then
-        break
+        return
     else
         selected_branch=$(echo "$selected_branch" | sed 's/^ *//' | sed 's/^\* //')
         git checkout "$selected_branch" >/dev/null 2>&1
@@ -39,7 +52,7 @@ choose_branch() {
 while true; do
     choice=$(show_main_menu)
     case "$choice" in
-        "Выбор ветки") choose_branch ;;
+        "Выбор ветки") change_branch ;;
         "Последние коммиты") echo "Здесь будет список последних коммитов" ;;
         "Коммит") echo "Здесь будет логика коммита"; read -p "Нажмите Enter, чтобы вернуться в меню" ;;
         "Выход") echo "Выход"; exit 0 ;;
