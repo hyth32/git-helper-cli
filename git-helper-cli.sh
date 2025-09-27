369349943e8336d70046be4d4b3fc7b24eb48ef1
Ivan Goryaev <5474n.sage@gmail.com>
2025-09-27 22:56:19 +0300

    добавил выбор веток

--- git-helper-cli.sh
+++ git-helper-cli.sh
@@ -1,35 +1,48 @@
 #! /bin/bash
 
 if ! git rev-parse --is-inside-work-tree &>/dev/null; then
-  echo "Текущая папка не является git репозиторием"
-  exit 1
+    echo "Текущая папка не является git репозиторием"
+    exit 1
 fi
 
+current_branch=$(git symbolic-ref --short HEAD)
+
+show_main_menu() {
+    actions=("Выбор ветки" "Последние коммиты" "Коммит" "Выход")
+    printf "%s\n" "${actions[@]}" | fzf --height 10 --border \
+        --reverse \
+        --header-first \
+        --header="Текущая ветка: $current_branch" \
+        --prompt="Выберите действие: "
+}
+
+choose_branch() {
+    branches=$(git branch --all)
+    branches="Назад"$'\n'"$branches"
+    
+    selected_branch=$(echo "$branches" | fzf --height 15 --border \
+        --reverse \
+        --header-first \
+        --header="Выбор ветки" \
+        --prompt="Выберите ветку: ")
+    
+    if [ -z "$selected_branch" ] || [ "$selected_branch" == "Назад" ]; then
+        break
+    else
+        selected_branch=$(echo "$selected_branch" | sed 's/^ *//' | sed 's/^\* //')
+        git checkout "$selected_branch" >/dev/null 2>&1
+        current_branch="$selected_branch"
+        echo "Переключено на ветку: $selected_branch"
+    fi
+}
+
 while true; do
-  choice=$(echo -e "Выбор ветки\nПоследние коммиты\nКоммит\nВыход" | fzf --height 10 --border --layout=reverse-list --prompt="Выберите действие: ")
-
-  case "$choice" in
-    "Выбор ветки")
-        branch=$(git branch --all | sed 's/^[ *]*//' | fzf --height 10 --border --layout=reverse-list --prompt="Выберите ветку: ")
-        if [ -n "$branch" ]; then
-          echo "Выбранная ветка: $branch"
-        fi
-        read -p "Нажмите Enter, чтобы вернуться в меню"
-        ;;
-    "Последние коммиты")
-        echo "Здесь будет список последних коммитов"
-        ;;
-    "Коммит")
-        echo "Здесь будет логика коммита"
-        read -p "Нажмите Enter, чтобы вернуться в меню"
-        ;;
-    "Выход")
-        echo "Выход"
-        exit 0
-        ;;
-    *)
-        echo "Неверный выбор"
-        ;;
-  esac
+    choice=$(show_main_menu)
+    case "$choice" in
+        "Выбор ветки") choose_branch ;;
+        "Последние коммиты") echo "Здесь будет список последних коммитов" ;;
+        "Коммит") echo "Здесь будет логика коммита"; read -p "Нажмите Enter, чтобы вернуться в меню" ;;
+        "Выход") echo "Выход"; exit 0 ;;
+        *) echo "Неверный выбор" ;;
+    esac
 done
-
