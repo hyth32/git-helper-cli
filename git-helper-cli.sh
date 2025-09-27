39c64839dab31b1d98bb294db62af24bd67a7d9d
Ivan Goryaev <5474n.sage@gmail.com>
2025-09-28 02:05:14 +0300

    добавил получение списка коммитов

--- git-helper-cli.sh
+++ git-helper-cli.sh
@@ -5,29 +5,42 @@ if ! git rev-parse --is-inside-work-tree &>/dev/null; then
     exit 1
 fi
 
+build_fzf_menu() {
+    local header="$1"
+    local prompt="$2"
+    shift 2
+    printf "%s\n" "$@" | fzf \
+        --height 10 \
+        --border \
+        --reverse \
+        --header-first \
+        --header="$header" \
+        --prompt="$prompt"
+}
+
 current_branch=$(git symbolic-ref --short HEAD)
 
 show_main_menu() {
-    actions=("Выбор ветки" "Последние коммиты" "Коммит" "Выход")
-    printf "%s\n" "${actions[@]}" | fzf --height 10 --border \
-        --reverse \
-        --header-first \
-        --header="Текущая ветка: $current_branch" \
-        --prompt="Выберите действие: "
+    actions=("Выбор ветки" "Список коммитов" "Коммит" "Выход")
+    build_fzf_menu \
+        "Текущая ветка: $current_branch" \
+        "Выберите действие: " \
+        "${actions[@]}"
 }
 
-choose_branch() {
+change_branch() {
     branches=$(git branch --all)
     branches="Назад"$'\n'"$branches"
-    
-    selected_branch=$(echo "$branches" | fzf --height 15 --border \
-        --reverse \
-        --header-first \
-        --header="Выбор ветки" \
-        --prompt="Выберите ветку: ")
+
+    selected_branch=$(
+        build_fzf_menu \
+        "Выбор ветки" \
+        "Выберите ветку: " \
+        "$branches"
+    )
     
     if [ -z "$selected_branch" ] || [ "$selected_branch" == "Назад" ]; then
-        break
+        return
     else
         selected_branch=$(echo "$selected_branch" | sed 's/^ *//' | sed 's/^\* //')
         git checkout "$selected_branch" >/dev/null 2>&1
@@ -36,11 +49,38 @@ choose_branch() {
     fi
 }
 
+show_commits() {
+    commits=$(git log --oneline)
+    commits="Назад"$'\n'"$commits"
+
+    selected_commit=$(
+        build_fzf_menu \
+            "Список коммитов (доступен скролл стрелками)" \
+            "Выберите коммит" \
+            "$commits"
+    )
+
+    if [ -z "$selected_commit" ] || [ "$selected_commit" == "Назад" ]; then
+        return
+    else
+        commit_hash=$(echo "$selected_commit" | awk '{print $1}')
+        clear
+        echo "Детали коммита $commit_hash:"
+        git show --stat --color=always "$commit_hash" | less -R
+    fi
+}
+
 while true; do
     choice=$(show_main_menu)
+
+    if [ -z "$choice" ]; then
+        echo "Выход"
+        exit 0
+    fi
+
     case "$choice" in
-        "Выбор ветки") choose_branch ;;
-        "Последние коммиты") echo "Здесь будет список последних коммитов" ;;
+        "Выбор ветки") change_branch ;;
+        "Список коммитов") show_commits ;;
         "Коммит") echo "Здесь будет логика коммита"; read -p "Нажмите Enter, чтобы вернуться в меню" ;;
         "Выход") echo "Выход"; exit 0 ;;
         *) echo "Неверный выбор" ;;
