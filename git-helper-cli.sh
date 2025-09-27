760c9d27c0ccef8d146cfc60adfa32b696c43787
Ivan Goryaev <5474n.sage@gmail.com>
2025-09-27 22:33:36 +0300

    добавил выбор веток

--- git-helper-cli.sh
+++ git-helper-cli.sh
@@ -1,20 +1,40 @@
 #! /bin/bash
 
 if ! git rev-parse --is-inside-work-tree &>/dev/null; then
-  echo "Текущая папка не является git репозиторием"
-  exit 1
+    echo "Текущая папка не является git репозиторием"
+    exit 1
 fi
 
 while true; do
-  choice=$(echo -e "Выбор ветки\nПоследние коммиты\nКоммит\nВыход" | fzf --height 10 --border --layout=reverse-list --prompt="Выберите действие: ")
+  current_branch=$(git symbolic-ref --short HEAD)
+
+  actions=("Выбор ветки" "Последние коммиты" "Коммит" "Выход")
+
+  choice=$(printf "%s\n" "${actions[@]}" \
+        | fzf --height 10 --border \
+              --reverse \
+              --header-first \
+              --header="Текущая ветка: $current_branch" \
+              --prompt="Выберите действие: " \
+  )
 
   case "$choice" in
     "Выбор ветки")
-        branch=$(git branch --all | sed 's/^[ *]*//' | fzf --height 10 --border --layout=reverse-list --prompt="Выберите ветку: ")
-        if [ -n "$branch" ]; then
-          echo "Выбранная ветка: $branch"
-        fi
-        read -p "Нажмите Enter, чтобы вернуться в меню"
+        while true; do
+            branches=$(git branch --all)
+            branches="Назад"$'\n'"$branches"
+
+            selected_branch=$(echo "$branches" | fzf --height 15 --border --reverse --prompt="Выберите ветку: ")
+
+            if [ "$selected_branch" == "Назад" ] || [ -z "$selected_branch" ]; then
+                break
+            else
+                selected_branch=$(echo "$selected_branch" | sed 's/^ *//' | sed 's/^\* //')
+                echo "Переключение на ветку $selected_branch"
+                git checkout "$selected_branch"
+                break
+            fi
+        done
         ;;
     "Последние коммиты")
         echo "Здесь будет список последних коммитов"
